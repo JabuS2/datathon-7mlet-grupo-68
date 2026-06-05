8cea4d67306543d9fbda896a7f5bfe88d75d973e
MatheusLucasAndrade <theuslucasandrade@gmail.com>
2026-06-05 20:24:50 -0300

    chore: removed unused file

--- src/core/auth.py
+++
@@ -1,9 +0,0 @@
-class Auth:
-    def __init__(self, secret_key: str, algorithm: str, access_token_expire_minutes: int):
-        self.secret_key = secret_key
-        self.algorithm = algorithm
-        self.access_token_expire_minutes = access_token_expire_minutes
-
-    def authenticate_user(self, usermail: str, password: str) -> bool:
-        # Implement your user authentication logic here
-        pass
